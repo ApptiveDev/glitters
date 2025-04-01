1949f6334241fd799db4fe1ce65f2a98057be015
mingkyeongg <ming0820@naver.com>
2025-04-02 01:05:14 +0900

    feat: commonInput 컴포넌트 생성

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,96 @@
+/* eslint-disable react/require-default-props */
+import React from 'react';
+import { DimensionValue, StyleSheet, Text, TextInput, TextInputProps, TextStyle, View } from 'react-native';
+
+interface CommonInputProps extends TextInputProps {
+  width?: DimensionValue;
+  height?: DimensionValue;
+  disable?: boolean;
+  fontSize?: number;
+  padding?: number;
+  backgroundColor?: string;
+  isValid?: boolean;
+  errorMessage?: string;
+  isError?: boolean;
+  isGuide?: boolean;
+  guideText?: string;
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    borderBottomWidth: 1,
+    marginBottom: 12,
+    paddingLeft: 8,
+    borderRadius: 4,
+    boxSizing: 'border-box',
+  },
+  errerText: {
+    color: '#FFA0A0',
+    paddingLeft: 8,
+    fontSize: 12,
+  },
+  component: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  guideText: {
+    color: '#FFF7C1',
+    fontSize: 12,
+    paddingLeft: 8,
+  },
+});
+
+const CommonInput = React.forwardRef<TextInput, CommonInputProps>(
+  (
+    {
+      width = '100%',
+      height = 'auto',
+      placeholder = 'Enter text',
+      defaultValue = '',
+      fontSize = 16,
+      padding = 12,
+      backgroundColor = 'transparent',
+      onChangeText = () => {},
+      disable = false,
+      isValid = true,
+      errorMessage = '',
+      isError = false,
+      isGuide = false,
+      guideText = '',
+      style,
+    },
+    ref,
+  ) => {
+    const dynamicStyles: TextStyle = {
+      width,
+      backgroundColor,
+      color: isValid ? '#8793C2' : '#FFF',
+      borderColor: disable ? '#8793C2' : '#FFF',
+      height,
+      fontSize,
+      padding,
+    };
+
+    return (
+      <View style={styles.component}>
+        <TextInput
+          style={[styles.input, dynamicStyles, style]}
+          placeholder={placeholder}
+          defaultValue={defaultValue}
+          onChangeText={onChangeText}
+          ref={ref}
+        />
+        {isError && (
+          <Text style={styles.errerText} disabled={!isError}>
+            {errorMessage}
+          </Text>
+        )}
+        {isGuide && <Text style={styles.guideText}>{guideText}</Text>}
+      </View>
+    );
+  },
+);
+
+export default CommonInput;
