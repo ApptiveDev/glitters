3b55585ff606f138d0812d474b73e58ba0bb35ee
mingkyeongg <ming0820@naver.com>
2025-04-02 01:05:14 +0900

    feat: commonInput 컴포넌트 생성

--- app/index.tsx
+++ app/index.tsx
@@ -17,7 +17,7 @@ export default function Index() {
   const onLayoutRootView = useCallback(async () => {
     if (isReady) {
       await SplashScreen.hideAsync();
-      router.replace('/maps');
+      router.replace('/login');
     }
   }, [isReady, router]);
 
