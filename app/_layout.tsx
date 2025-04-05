086170e60549844db014841ba9c8e1a5aa285004
mingkyeongg <ming0820@naver.com>
2025-04-06 00:45:56 +0900

    feat: SafeAreaView 적용

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,17 +2,22 @@ import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Slot } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import colors from '@/types/colors';
 
 const queryClient = new QueryClient();
 
 export const RootLayout = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <BottomSheetModalProvider>
-          <Slot />
-        </BottomSheetModalProvider>
-      </GestureHandlerRootView>
+      <SafeAreaView style={{ flex: 1, backgroundColor: `${colors.background}` }}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <BottomSheetModalProvider>
+            <Slot />
+          </BottomSheetModalProvider>
+        </GestureHandlerRootView>
+      </SafeAreaView>
     </QueryClientProvider>
   );
 };
