9d90a99e2060eaca98fbdb709a4f46ff87c5c3bf
mingkyeongg <ming0820@naver.com>
2025-05-20 02:23:50 +0900

    fix: config 환경변수 이름 변경

--- app.config.js
+++ app.config.js
@@ -23,7 +23,7 @@ export default {
       },
       config: {
         googleMaps: {
-          apiKey: process.env.GOOGLE_MAPS_API_KEY,
+          apiKey: process.env.GOOGLE_API_KEY,
         },
       },
       package: 'me.banjjak.Banjjak',
