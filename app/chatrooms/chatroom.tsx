a4d9079e789ff53e0a9d491ff6f2902919ec1fef
mingkyeongg <ming0820@naver.com>
2025-05-26 03:30:45 +0900

    feat: 절댓값으로 변경

--- app/chatrooms/chatroom.tsx
+++ app/chatrooms/chatroom.tsx
@@ -327,7 +327,7 @@ export const Chatroom = () => {
         </ScrollView>
         <View
           style={{
-            paddingBottom: Platform.OS === 'android' ? keyboardHeight - 120 : 0,
+            paddingBottom: Platform.OS === 'android' ? 16 : 0,
           }}
         >
           <ChatInput chatroomId={selectedChatroom?.id || 0} onSend={handleSend} />
